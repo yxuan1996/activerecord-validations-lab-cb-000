24d33940c87c69b6951b09d64146895f437ee197
Yi Xuan Sim <yxuan1996@gmail.com>
2017-10-26 00:40:58 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,6 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
+  validates :category, inclusion:
 
 end
