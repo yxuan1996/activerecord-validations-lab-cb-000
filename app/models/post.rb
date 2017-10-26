8bb51f3f5f536661cca497166682dc8ed842533c
Yi Xuan Sim <yxuan1996@gmail.com>
2017-10-26 00:45:25 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :is_clickbait
 
   CLICKBAIT_PATTERNS = [
     /Won't Believe/i,
